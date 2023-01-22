b00978c9273898dd93a8c4273327e796f04cb58f
lukoyanov.egor <lukoyanov.egor@mail.ru>
2023-01-23 00:45:43 +0300

    Home task 2 added to repo and changed file for home task 1

---
+++ HomeTask2/ipython_log.py
@@ -0,0 +1,80 @@
+# IPython log file
+
+bin(1234567)
+#[Out]# '0b100101101011010000111'
+a = 0b00110011 | 0b01000000
+bin(a)
+#[Out]# '0b1110011'
+(a > -10) and (a < 100)
+#[Out]# False
+42 << 34
+#[Out]# 721554505728
+int(13.8)
+#[Out]# 13
+13.888.__ceil__()
+13.888.__dir__()
+#[Out]# ['__repr__',
+#[Out]#  '__hash__',
+#[Out]#  '__getattribute__',
+#[Out]#  '__lt__',
+#[Out]#  '__le__',
+#[Out]#  '__eq__',
+#[Out]#  '__ne__',
+#[Out]#  '__gt__',
+#[Out]#  '__ge__',
+#[Out]#  '__add__',
+#[Out]#  '__radd__',
+#[Out]#  '__sub__',
+#[Out]#  '__rsub__',
+#[Out]#  '__mul__',
+#[Out]#  '__rmul__',
+#[Out]#  '__mod__',
+#[Out]#  '__rmod__',
+#[Out]#  '__divmod__',
+#[Out]#  '__rdivmod__',
+#[Out]#  '__pow__',
+#[Out]#  '__rpow__',
+#[Out]#  '__neg__',
+#[Out]#  '__pos__',
+#[Out]#  '__abs__',
+#[Out]#  '__bool__',
+#[Out]#  '__int__',
+#[Out]#  '__float__',
+#[Out]#  '__floordiv__',
+#[Out]#  '__rfloordiv__',
+#[Out]#  '__truediv__',
+#[Out]#  '__rtruediv__',
+#[Out]#  '__new__',
+#[Out]#  'conjugate',
+#[Out]#  '__trunc__',
+#[Out]#  '__round__',
+#[Out]#  'as_integer_ratio',
+#[Out]#  'fromhex',
+#[Out]#  'hex',
+#[Out]#  'is_integer',
+#[Out]#  '__getnewargs__',
+#[Out]#  '__getformat__',
+#[Out]#  '__set_format__',
+#[Out]#  '__format__',
+#[Out]#  'real',
+#[Out]#  'imag',
+#[Out]#  '__doc__',
+#[Out]#  '__str__',
+#[Out]#  '__setattr__',
+#[Out]#  '__delattr__',
+#[Out]#  '__init__',
+#[Out]#  '__reduce_ex__',
+#[Out]#  '__reduce__',
+#[Out]#  '__subclasshook__',
+#[Out]#  '__init_subclass__',
+#[Out]#  '__sizeof__',
+#[Out]#  '__dir__',
+#[Out]#  '__class__']
+13.888.__round__()
+#[Out]# 14
+a = 13.888
+b = 13.5
+((a - b >= 0) and (a - b <= 0.5))
+#[Out]# True
+(a - 13.888 < 0.0000000001)
+#[Out]# True
